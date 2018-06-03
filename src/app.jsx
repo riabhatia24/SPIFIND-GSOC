b0e018aaea21c010aba337b80d55b493e9360987
Ria Bhatia <riabhatia24@gmail.com>
2018-06-04 02:53:11 +0530

    Updated

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import Level1 from './levels/Level1';
-import Level2 from './levels/Level2';
-import Level3 from './levels/Level3';
-import Level4 from './levels/Level4';
-import Level5 from './levels/Level5';
+
 import 'normalize.css';
 
 import "styles/base/_main.sass"  // Global styles
