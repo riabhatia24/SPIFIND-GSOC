cd178e2f73b28e96c2b1bcf7c954ef0ea2dfda0c
Ria Bhatia <riabhatia24@gmail.com>
2018-08-10 17:22:20 +0530

    Gameover change

--- src/score/Gameover.js
+++ src/score/Gameover.js
@@ -16,7 +16,7 @@ export default class Gameover1 extends React.Component{
 	return <div style={{backgroundColor: '#111E6C',
       height: '650px',
       position: 'absolute',
-      width: '1420px', left: '-670px', top: '-630px', borderRadius: '10px'}}><h1 className={'info'}>Game Over</h1>
+      width: '1440px', left: '-620px', top: '-630px', borderRadius: '10px'}}><h1 className={'info'}>Game Over</h1>
        <p style={{textAlign: 'center', fontSize: '40px', fontFamily: 'georgia', margin: '0', padding: '0'}}>Your Score is {this.props.score}</p>
         <Link to = "/"><button style = {{marginLeft: '520px', marginTop: '30px', width: '400px', height: '90px'}} className = {'mainbutton2'} onClick = {(e) => e.stopPropagation()}>Go to Main Menu</button></Link>
         <img src = {Spider} style = {{marginLeft: '570px'}} height = "300" width = "300"/> 
